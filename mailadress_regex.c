ba14d493c55bc875c59facced8c042730503e8d0
Okakakai <kai48586@gmail.com>
2021-06-08 15:27:28 +0900

    メールアドレス完成

--- mailadress_regex.c
+++ mailadress_regex.c
@@ -2,46 +2,48 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(int count, char **fname) {
+int main(int count, char **fname)
+{
     FILE *fp;
-    char str[1024], result[14], buffer[512];
+    char str[1024], result[1024], buffer[512];
 
-    const char regex[] = "[\\w_\\-.]+@[\\w_\\-.]+\\.[A-Za-z]+";
-    // \.  メタキャラクタなので\をつける　ｎと\nみたいな
-    // \w アルファベット、_ ,数字
-    // + 直前のやつが一回以上
-
+    const char regex[] = "[A-Za-z0-9\\-\\.\\_\\!\\#\\$\\%\\&\\'\\*\\+\\/\\=\\?\\^\\{\\}\\~\\|]+@[A-Za-z0-9\\-\\.\\_\\!\\#\\$\\%\\&\\'\\*\\+\\/\\=\\?\\^\\{\\}\\~\\|]+\\.[A-Za-z]+";
     regex_t preg;
     regmatch_t pm[1];
 
     int firstindex, lastindex;
 
-    if (regcomp(&preg, regex, REG_EXTENDED | REG_NEWLINE) != 0) {
+    if (regcomp(&preg, regex, REG_EXTENDED | REG_NEWLINE) != 0)
+    {
         printf("regex is not avairable\n");
         return 1;
     }
 
     fp = fopen(fname[1], "r");
 
-    if (fp == NULL) {
+    if (fp == NULL)
+    {
         printf("did not file open\n");
         return 1;
     }
 
-    while (fgets(str, 1024, fp) != NULL) {
-        if (regexec(&preg, str, 1, pm, 0) == 0) {
+    while (fgets(str, 1024, fp) != NULL)
+    {
+        if (regexec(&preg, str, 1, pm, 0) == 0)
+        {
             firstindex = pm[0].rm_so;
             lastindex = pm[0].rm_eo;
             strncpy(result, str + firstindex, lastindex - firstindex);
             strcpy(buffer, str);
             strncpy(str, buffer + lastindex,
                     (sizeof buffer / sizeof buffer[1]) - lastindex);
-            printf("検索がヒットしました\n\n電話番号 : %s\n", result);
-        } else {
+            printf("検索がヒットしました\nメールアドレス : %s\n\n", result);
+        }
+        else
+        {
             printf("...\n");
         }
     }
-
     printf("検索終了\n");
     regfree(&preg);
     fclose(fp);
